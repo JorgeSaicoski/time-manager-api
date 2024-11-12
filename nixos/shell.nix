18e3b3c208d4ed28ce260209e22e896462328a88
Jorge Saicoski <jorgesaicoski@gmail.com>
2024-11-12 12:47:52 -0300

    nixos

---
+++ nixos/shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.go              # Go programming language
+    pkgs.postgresql      # PostgreSQL database
+    pkgs.podman          # Podman for container management
+    pkgs.git             # Git for version control, if needed
+  ];
+
+  # Set up Go environment variables
+  shellHook = ''
+    export GOPATH=$HOME/go
+    export PATH=$GOPATH/bin:$PATH
+    echo "Go, Gorm, PostgreSQL, and Podman environment loaded!"
+  '';
+}
