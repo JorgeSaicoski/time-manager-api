dc927ab17550bd0f677c5e79c3d53f6d3f72ef97
JorgeSaicoski <jorgesaicoski@gmail.com>
2024-11-26 15:42:03 -0300

    break repo ready

--- internal/repository/break.go
+++ internal/repository/break.go
@@ -1,17 +1,41 @@
 package repository
 
 import (
+	"time"
+
 	"github.com/JorgeSaicoski/time-manager-api/internal/models"
 )
 
-func (r *Repository) GetRunningBreak(userID int64) (*models.BreakTime, error){
-  var break models.BreakTime
-  if err := GetCurrentItem(userID, &break); err!=nil{
-    return nil, err
-  }
-  if break.ID == nil {
-    return nil, nil
-  }
+func (r *Repository) GetRunningBreak(userID int64) (*models.BreakTime, error) {
+	var breakTime models.BreakTime
+	if err := GetCurrentItem(userID, &breakTime); err != nil {
+		return nil, err
+	}
+	if breakTime.ID == nil {
+		return nil, nil
+	}
 
-	return &break, nil  
+	return &breakTime, nil
+}
+
+func (r *Repository) StopCurrentBreak(userID int64) error {
+	breakTime, err := r.GetRunningBreak(userID)
+	if err != nil {
+		return fmt.Errorf("error finding break time: %w", err)
+	}
+
+	if breakTime == nil {
+		return nil
+	}
+
+	breakTime.Closed = true
+	now := time.Now()
+	diff := now.Sub(breakTime.StartTime)
+	breakTime.Duration.Add(diff)
+
+	return r.db.Save(breakTime).Error
+}
+
+func (r *Repository) CreateBreakTime(bt *models.TotalTime) error {
+	return r.db.Create(bt).Error
 }
